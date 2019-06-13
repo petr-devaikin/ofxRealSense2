9b23ff53b2f840ee21660c650679cb3fd7bdc46f
Petr Devaikin <p.devaikin@gmail.com>
2019-06-13 21:19:35 +0200

    finally init

---
+++ src/ofxLibRealSense2.cpp
@@ -0,0 +1,34 @@
+//
+//  ofxLibRealSense2.cpp
+//
+//  Created by Petr Devaikin on 2019/06/13
+//
+
+#include "ofxLibRealSense2.hpp"
+
+
+void ofxLibRealSense2::setup(int width, int height, RealSenseChannel channels) {
+    if (channels & Color) {
+        //lastColorPixels
+    }
+}
+
+bool ofxLibRealSense2::start() {
+    
+}
+
+void ofxLibRealSense2::stop() {
+    
+}
+
+&ofPixels ofxLibRealSense2::getColorPixels() {
+    
+}
+
+&ofPixels ofxLibRealSense2::getIrPixels() {
+    
+}
+
+&ofPixels ofxLibRealSense2::getDepthPixels() {
+    
+}
